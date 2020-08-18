794205d8e1839d6d0164230b4bcec7d76a9c68b3
Roberto Peruzzo <roberto.peruzzo@studioaqua.it>
2020-08-18 22:13:11 +0200

    Added local error reporting settings.

--- scaffold/tpl.settings.local.php
+++ scaffold/tpl.settings.local.php
@@ -14,9 +14,14 @@ $base_url = 'http://drupal.docker.localhost:8000';
 
 // Enable display all errors.
 $conf['error_level'] = 2;
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
 
 // Local temporary directory.
 $conf['file_temporary_path'] = '/tmp';
+// Local private directory.
+$conf['file_private_path'] = '../private/files';
 
 $conf['drupal_http_request_fails'] = FALSE;
 
@@ -24,5 +29,3 @@ $conf['drupal_http_request_fails'] = FALSE;
 $conf['reroute_email_address'] = 'roberto.peruzzo@studioaqua.it';
 
 $conf['theme_debug'] = TRUE;
-
-$conf['file_private_path'] = '../private/files';
